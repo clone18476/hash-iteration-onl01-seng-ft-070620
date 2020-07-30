7cdcc364c41f0563c285e7961d8eb8ce1a1d9fac
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-30 18:37:45 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,11 +8,15 @@
 
 def select_winner(passengers)
   winner = ""
-passengers.each do |suite, name|
+passengers.each do |suite, name|  
+    # iterate thru hash using each cause we just want the name of the winner 
   if suite == :suite_a && name.start_with?("A")
+      # use if statement that returns true if it's the right suite AND if the person in that suite's name starts with "A"
     winner = name
+     # if that name returns true it is the winner; set the name equal to the winner variable and end iteration 
   end
 end
  
 winner
+   # call winner variable to return name of winner 
 end
